f58b43d4684a707fa3ca8f4a983cdcd4d1d2c448
evanbarg <98602426+evanbarg@users.noreply.github.com>
2023-10-14 03:32:58 -0400

    Filters by Alphanumeric and Numeric

--- script.ps1
+++ script.ps1
@@ -1,7 +1,7 @@
 #path to file to read
 $data = Get-Content -Path "C:\Users\evan\Desktop\sample.txt"
 
-#Filter by Alpha
+#Filter by Alpha or Numeric
 $alphaValues = @()
 $data | ForEach-Object {
     $values = $_.Split(',')
@@ -9,10 +9,16 @@ $data | ForEach-Object {
         if ($value -match '^[a-zA-Z]+$') {
             $alphaValues += $value
         }
+        elseif ($value -match '^\d+$') {
+            $numericValues += $value
+        }
     }
 }
 
 
 #Print Alpha
 $alphaValues | ForEach-Object {Write-Output $_} 
+
+#Print Numeric
+$numericValues | ForEach-Object {Write-Output $_}
 
