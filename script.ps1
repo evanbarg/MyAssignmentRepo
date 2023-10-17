80fa7548a2f7a6f7dbac2bf562a7e9b6d411c020
evanbarg <98602426+evanbarg@users.noreply.github.com>
2023-10-17 03:59:00 -0400

    added both filter parameter
    uses a switch now opposed to if statements to filter

--- script.ps1
+++ script.ps1
@@ -5,57 +5,30 @@ if (-not (Test-Path $filePath)) {
     Write-Output "Not a valid file path"
     exit
 }
+$filterType = Read-Host "Enter the filter type: 'Numeric', 'Alphabet', or 'Both'"
+$sortOrder = Read-Host "Enter sort order: 'Ascending' or 'Descending'"
 
-$data = Get-Content -Path $filePath
-
-#Filter by Alpha or Numeric
-$alphaValues = @()
-$numericValues = @()
-$data | ForEach-Object {
-    $values = $_.Split(',')
-    ForEach ($value in $values) {
-        if ($value -match '^[a-zA-Z]+$') {
-            $alphaValues += $value
-        }
-        elseif ($value -match '^\d+$') {
-            $numericValues += $value
-        }
+$fileContent = Get-Content $filePath
+#filter the contnet
+switch ($filterType) {
+    'numeric' {
+        $filteredContent = $fileContent | Where-Object { $_ -match '^\d+$' }
+    }
+    'alphabetic' {
+        $filteredContent = $fileContent | Where-Object { $_ -match '^[a-zA-Z]+$' }
+    }
+    'both' {
+        $filteredContent = $fileContent | Where-Object { $_ -match '^[a-zA-Z0-9]+$' }
     }
 }
 
-#Ask for User Input
-$userInput = Read-Host -Prompt "Enter 'numeric' to see numbers or 'alphabet' to see letters, or 'both' to see numebers and letters"
-$sortOrder = Read-Host - Prompt "Enter 'ascending' to sort ascending order or enter 'descending' to sort descending order"
-
-#Print output
-#Print both
-if ($userInput -eq "both") {
-    if ($sortOrder -eq "ascending") {
-        $value | Sort-Object | ForEach-Object {Write-Output $_ }
-    } elseif ($sortOrder -eq "descending") {
-        $value | Sort-Object -Descending | ForEach-Object {Write-Output $_ }
-    } else {
-        Write-Output "Please enter 'ascending' or descending'"
-}
-}
-#Numeric
-if ($userInput -eq "numeric") {
-    if ($sortOrder -eq "ascending") {
-        $numericValues | Sort-Object | ForEach-Object {Write-Output $_ }
-    } elseif ($sortOrder -eq "descending") {
-        $numericValues | Sort-Object -Descending | ForEach-Object {Write-Output $_ }
-    } else {
-        Write-Output "Please enter 'ascending' or descending'"
-    } #Alphabet
-} elseif ($userInput -eq "alphabet") {
-    if ($sortOrder -eq "ascending") {
-        $alphaValues | Sort-Object | ForEach-Object {Write-Output $_ }
-    } elseif ($sortOrder -eq "descending") {
-        $alphaValues | Sort-Object -Descending | ForEach-Object {Write-Output $_ }
-    } else {
-        Write-Output "Invalid sort order. Please enter 'ascending' or 'descending'."
-    }
+#Sort based on sort input
+if ($sortOrder -eq 'descending') {
+    $filteredContent = $filteredContent | Sort-Object -Descending
 } else {
-    Write-Output "Invalid input. Please enter 'numeric' or 'alphabetic'."
+    $filteredContent = $filteredContent | Sort-Object
 }
 
+#outputs comma seperated
+$filteredContentComma = $filteredContent -join ","
+Write-Output $filteredContentComma
